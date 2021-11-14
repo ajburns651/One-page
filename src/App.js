1485c06b3ed22483c4b92c0e3f79dc01d41f9330
ajburns651 <ajburns651@gmail.com>
2021-11-14 11:16:10 -0800

    links,images,tempature

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ export default App;
 const Wrapper = styled.div`
     padding: 10rem;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    align-items: stretch;
     background-color: var(--bg-color);
     height: 100vh;
     padding: 12rem 20rem;
@@ -59,5 +60,5 @@ const Container = styled.div`
     display: flex;
     flex: 2;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: center;
 `;
